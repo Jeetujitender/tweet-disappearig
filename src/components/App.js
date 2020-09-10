1252400f9898cd751d48056749b08e43ac5a4271
imcodekhan <imkhan.mdamir@outlook.com>
2020-09-10 16:08:49 +0530

    redux code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,53 @@
-import React from 'react';
-import { Layout, Menu, Button } from 'antd';
+import React, { useState } from "react";
+import { Layout, Menu, Button } from "antd";
 import "antd/dist/antd.css";
-import DateTime from './DateTime';
-import TweetField from './TweetField';
-import TweetList from './TweetList';
-import { addTweet } from '../actions';
+import DateTime from "./DateTime";
+import TweetField from "./TweetField";
+import TweetList from "./TweetList";
+import { addTweet } from "../actions";
+import { connect } from "react-redux";
 
 const { Header } = Layout;
 
-const App = () => {
-    return (
+const App = ({ dispatch }) => {
+  const [tweet, setTweet] = useState("");
+  const [duration, setDuration] = useState("");
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <TweetField setTweet={setTweet} />
 
+          <DateTime
+            style={{ margin: "0 0 0 50px" }}
+            setDuration={setDuration}
+          />
+          <Button
+            style={{ width: 100, margin: "5px 50px" }}
+            onClick={() => dispatch(addTweet(tweet, duration))}
+            type="primary"
+            shape="round"
+          >
+            Tweet
+          </Button>
+        </div>
 
-
-        < Layout className="layout" >
-            <Header>
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
-                </Menu>
-                <div style={{ display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column" }}>
-                    <TweetField />
-
-                    <DateTime style={{ margin: "0 0 0 50px" }} />
-                    <Button style={{ width: 100, margin: "5px 50px" }} onClick={dispatch("ADD_TWEET")} type="primary" shape="round">Tweet</Button>
-                </div>
-
-                <TweetList />
-            </Header>
-
-
-        </Layout >
-    );
-}
-export default App;
-
-
-
-
+        <TweetList />
+      </Header>
+    </Layout>
+  );
+};
+export default connect()(App);
--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import { connect } from 'react-redux';
-// import { addTweet } from "../actions"
+import React from "react";
 
-import { Card } from 'antd';
+import { Card } from "antd";
+import { connect } from "react-redux";
+import { deleteTweet } from "../actions";
 const TweetList = (props) => {
-    const renderList = () => {
-        return (
-            props.tweets.map(tweet => {
-                return (
-                    <div className="site-card-border-less-wrapper">
-                        <Card title="Disappearing time :  {tweet.duration} " bordered={false} style={{ width: 600 }}>
-                            <p>{tweet.tweet_text} </p>
-
-
-                        </Card>
-                    </div>
-                );
-
-            })
-        )
-    };
-    return <div>{renderList()}</div>
-}
-const mapStateToProps = state => {
-    console.log(state.Tweet)
-    return { tweets: state.Tweet }
+  const renderList = () => {
+    return props.tweets.map((tweet) => {
+      return (
+        <div className="site-card-border-less-wrapper">
+          <Card
+            title={`Disappearing time :  ${tweet.duration}`}
+            bordered={false}
+            style={{ width: 600 }}
+          >
+            <p>{tweet.tweet} </p>
+            <button onClick={() => props.dispatch(deleteTweet(tweet.id))}>
+              delete
+            </button>
+          </Card>
+        </div>
+      );
+    });
+  };
+  return <div>{renderList()}</div>;
 };
+
+const mapStateToProps = (state) => ({
+  tweets: state.tweets,
+});
 
 export default connect(mapStateToProps)(TweetList);
